55b12a2ddb7f176ad773b55693cafa731b254201
Susanta Maity <119559211+Susanta252@users.noreply.github.com>
2022-12-01 10:56:20 +0530

    Add files via upload

---
+++ java/Addition.java
@@ -0,0 +1,9 @@
+public class Addition
+{
+public static void main(String args[])
+{
+int a=10,b=8;
+int sum = a + b;
+System.out.println(sum);
+}
+}
---
+++ java/Substraction.java
@@ -0,0 +1,9 @@
+public class Substraction
+{
+public static void main(String args[])
+{
+int a=20,b=8;
+int c=a-b;
+System.out.println(c);
+}
+}
