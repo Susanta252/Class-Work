b388dbf2d2c7d9efdfaffc6a69e9791b31a25a9f
Susanta Maity <119559211+Susanta252@users.noreply.github.com>
2022-12-05 14:07:56 +0530

    Add files via upload

---
+++ Project2.java
@@ -0,0 +1,15 @@
+public class Project2{
+int id = 100;
+String name = "Susanta";
+String address = "Salkia Howrah";
+long phno = 7432561819L;
+static String college = "Swami Vivekananda University";
+public static void main(String[] args){
+Project2 pj = new Project2();
+System.out.println(pj.id);
+System.out.println(pj.name);
+System.out.println(pj.address);
+System.out.println(pj.college);
+System.out.println(pj.phno);
+}
+}
